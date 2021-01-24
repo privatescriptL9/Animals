149417630a7c65f2663a50fa43312d04f941370b
Ant <toscha-ilyin2012@yandex.ru>
2021-01-24 19:17:07 +0300

    hover effect for cards

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,9 @@
 
-const container__area = document.querySelectorAll('.container__area')
+// const container__area = document.querySelectorAll('.container__area')
+// const area__title = document.querySelector('.area__title')
 
-container__area.forEach(element => {
-  element.addEventListener('mouseover', () => {
-    console.log('MouseOver')
-  })
-})
+// container__area.forEach(area => {
+//   area.addEventListener('mouseenter', (e) => {
+//     container__area.style.flex = '0'
+//   })
+// })
