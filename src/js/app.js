c2b8bc0996408cc3a872cdeeede739e7cf38984c
Slavya <slava.larkin.96@gmail.com>
2021-01-26 22:55:00 +0300

    upload photos some regions and add files css

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,12 @@
 const container__area = document.querySelectorAll('.container__area')
 
-const urls = ['taiga.html', 'desert.html', 'savannah.html', 'steppe.html', 'tropics.html']
+const urls = [
+  'taiga.html',
+  'desert.html',
+  'savannah.html',
+  'steppe.html',
+  'tropics.html',
+]
 
 container__area.forEach((element, index) => {
   element.addEventListener('click', () => {
