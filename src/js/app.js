6b8573c3d73a01e1f457deec0cfc0efd5ad67348
Ant <toscha-ilyin2012@yandex.ru>
2021-01-26 22:57:48 +0300

    animal for taiga

--- src/js/app.js
+++ src/js/app.js
@@ -10,4 +10,11 @@ container__area.forEach((element, index) => {
 
 new fullpage('#fullpage', {
   autoScrolling: true,
+  navigation: true,
+  continuousHorizontal: true,
+  scrollHorizontally: true,
+  afterResponsive: function(isResponsive){},
 })
+
+fullpage_api.cards.setOption('perspective', 500);
+fullpage_api.cards.init();
