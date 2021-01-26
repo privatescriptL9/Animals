2be5b1644428f957fa509b73d9c2e44aa65a3d86
Ant <toscha-ilyin2012@yandex.ru>
2021-01-26 20:37:56 +0300

    hs chto

--- src/js/app.js
+++
@@ -1,9 +0,0 @@
-
-// const container__area = document.querySelectorAll('.container__area')
-// const area__title = document.querySelector('.area__title')
-
-// container__area.forEach(area => {
-//   area.addEventListener('mouseenter', (e) => {
-//     container__area.style.flex = '0'
-//   })
-// })
